28f4e14314f98ceada28e741e3d811435f7255f6
Divyanka Chaudhari <60309618+Iceberry25@users.noreply.github.com>
2020-04-25 09:41:31 +0530

    Update Classifying Iris Species.py

--- Classifying Iris Species.py
+++ Classifying Iris Species.py
@@ -1,6 +1,7 @@
 from sklearn.datasets import load_iris
 iris_dataset = load_iris()
 
+#Print the feature names for understanding what each variable contains 
 print("Keys of iris_dataset:\n", iris_dataset.keys())
 print(iris_dataset['DESCR'][:193] + "\n...")
 print("Target names:", iris_dataset['target_names'])
@@ -24,5 +25,40 @@ print("y_test shape:", y_test.shape)
 # create dataframe from data in X_train
 # label the columns using the strings in iris_dataset.feature_names
 iris_dataframe = pd.DataFrame(X_train, columns=iris_dataset.feature_names)
+
 # create a scatter matrix from the dataframe, color by y_train
 pd.plotting.scatter_matrix(iris_dataframe, c=y_train, figsize=(15, 15), marker='o', hist_kwds={'bins': 20}, s=60, alpha=.8, cmap=mglearn.cm3)
+
+
+# Building KNN model
+from sklearn.neighbors import KNeighborsClassifier
+knn = KNeighborsClassifier(n_neighbors=1)
+
+# Fitting the data 
+knn.fit(X_train, y_train)
+
+# Making predictions
+X_new = np.array([[5, 2.9, 1, 0.2]])
+print("X_new.shape:", X_new.shape)
+
+prediction = knn.predict(X_new)
+print("Prediction:", prediction)
+print("Predicted target name:",
+       iris_dataset['target_names'][prediction])
+
+#Evaluating the model
+
+y_pred = knn.predict(X_test)
+print("Test set predictions:\n", y_pred)
+
+print("Test set score: {:.2f}".format(np.mean(y_pred == y_test)))
+print("Test set score: {:.2f}".format(knn.score(X_test, y_test)))
+
+# summary and outlook
+X_train, X_test, y_train, y_test = train_test_split(
+    iris_dataset['data'], iris_dataset['target'], random_state=0)
+
+knn = KNeighborsClassifier(n_neighbors=1)
+knn.fit(X_train, y_train)
+
+print("Test set score: {:.2f}".format(knn.score(X_test, y_test)))
